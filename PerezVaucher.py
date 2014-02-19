36f88c42e89c2913df037873dc69f87d89ce4a00
Alexandre Perez <alexandre.perez92@gmail.com>
2014-02-19 23:26:23 +0100

    Refactor, add missing methods for GA, change solution structure to array (was tuple before) because we need to modify the eval value of a solution

--- PerezVaucher.py
+++ PerezVaucher.py
@@ -5,17 +5,21 @@ import sys
 import time
 from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN
 
-cities = []
+# GUI
 screen = None
-city_color = (10,10,200) # blue
+city_color = (10, 10, 200) # blue
 summary_color = (255, 255, 255)
 city_font_size = 16 # pixels
 summary_font_size = 26
 screen_size = (500, 500)
 city_radius = 2
-elitism = 4
 city_font = None
 summary_font = None
+
+cities = []
+elitism_percent = 30 / 100
+population_size = 100
+
 
 def citiesFromFile(file):
     f = open(file, 'r')
@@ -27,7 +31,8 @@ def citiesFromFile(file):
 
     return cities
 
-def drawCities(positions, connected = False, generation = -1, distance = -1):
+
+def drawCities(positions, connected=False, generation=-1, distance=-1):
     if screen == None:
         print(positions)
     else:
@@ -43,13 +48,15 @@ def drawCities(positions, connected = False, generation = -1, distance = -1):
             pygame.draw.lines(screen, city_color, True, [x[1] for x in cities])
 
         if generation != -1 and distance != -1:
-            text = city_font.render('Generation n°' + str(generation) + '; Distance = ' + str(round(distance, 2)), True, summary_color, (0, 0, 0))
+            text = city_font.render('Generation n°' + str(generation) + '; Distance = ' + str(round(distance, 2)), True,
+                                    summary_color, (0, 0, 0))
             textrect = text.get_rect()
             textrect.centerx = screen_size[0] / 2.0
             textrect.centery = screen_size[1] - summary_font_size / 4.0
             screen.blit(text, textrect)
 
         pygame.display.flip()
+
 
 def setupGui():
     global screen, city_font, summary_font
@@ -67,7 +74,6 @@ def citiesByMouse():
     if screen == None:
         wasGui = False
         setupGui()
-
 
     collecting = True
     i = 0
@@ -87,33 +93,51 @@ def citiesByMouse():
         pygame.quit()
         screen = None
 
+
 def ga_solve(file=None, gui=True, maxtime=0):
     global cities
-    
+    t1 = time.time()
+
     if gui:
         setupGui()
-    
-    if file != None:
+
+    if file is not None:
         cities = citiesFromFile(file)
     else:
         citiesByMouse()
     drawCities(cities)
 
-    population = initial_population(cities, 12)
-    t1 = time.time()
+    # Initial population
+    population = initial_population(cities, population_size)
+
     gen = 0
+    fittest = None
+
     while maxtime == 0 or time.time() - t1 <= maxtime:
+        # Evaluate
+        evaluate(population)
+        # Check the fittest
+        if fittest is None or fittest[1] > population[0][1]:
+            fittest = population[0].copy()
+            drawCities(fittest[0], True, gen, fittest[1])
+        # Sort for selection
         population.sort(key=lambda s: s[1])
+        # Selection
         elites = []
-        for i in range(0,elitism):
+        for i in range(0, int(len(population) * elitism_percent)):
             elites.append(population[i])
+        # Crossover
         childs = cross(elites)
         for i in range(len(population) - len(childs), len(population)):
             population[i] = childs[len(population) - i - len(childs)]
-        print(total_distance(cities))
-        initial_population(cities)
-        drawCities(cities, True, gen, total_distance(cities))
+        # Mutate
+        mutate(population[random.randint(0, len(population) - 1)][0])
         gen += 1
+
+
+def evaluate(population):
+    for solution in population:
+        solution[1] = total_distance(solution[0])
 
 
 def cross(subpopulation):
@@ -130,14 +154,14 @@ def cross(subpopulation):
             temp = p2
             p2 = p1
             p1 = temp
-        if s1 != None and s2 != None:
+        if s1 is not None and s2 is not None:
             childs = cross_two_solutions(s1, s2, p1, p2)
             crossed += childs
 
     return crossed
 
+
 def cross_two_solutions(solution1, solution2, p1, p2):
-    childs = []
     middle_cities1 = []
     middle_cities2 = []
     for i in range(p1, p2):
@@ -151,18 +175,30 @@ def cross_two_solutions(solution1, solution2, p1, p2):
         solution1[i] = middle_cities2[i - p1]
         solution2[i] = middle_cities1[i - p1]
 
-    solution1 = (solution1, total_distance(solution1))
-    solution2 = (solution2, total_distance(solution2))
+    solution1 = [solution1, None]
+    solution2 = [solution2, None]
 
     if is_solutions_equal(solution1, solution2):
         return [solution1]
     return [solution1, solution2]
 
+
+def mutate(solution):
+    p1 = random.randint(0, len(solution) - 1)
+    p2 = p1
+    while p1 == p2:
+        p2 = random.randint(0, len(solution) - 1)
+    temp = solution[p1]
+    solution[p1] = solution[p2]
+    solution[p2] = temp
+
+
 def is_solutions_equal(solution1, solution2):
-    for i in range(0 , len(solution1)):
+    for i in range(0, len(solution1)):
         if solution1[0][i][0] != solution2[0][i][0]:
             return False
     return True
+
 
 def pack_cities(solution, middle_cities, p1, p2):
     for i in range(0, len(solution)):
@@ -170,9 +206,9 @@ def pack_cities(solution, middle_cities, p1, p2):
             solution[i] = None
     i = p2
     while i != p1:
-        if solution[i] == None:
+        if solution[i] is None:
             j = i + 1
-            while j >= len(solution) - 1 or solution[j] == None:
+            while j >= len(solution) - 1 or solution[j] is None:
                 if j >= len(solution) - 1:
                     j = 0
                 else:
@@ -184,27 +220,30 @@ def pack_cities(solution, middle_cities, p1, p2):
         else:
             i += 1
 
+
 def total_distance(cities):
     distance = 0
     c2 = None
     for c in cities:
         c1 = c2
         c2 = c
-        if c1 != None and c2 != None:
+        if c1 is not None and c2 is not None:
             distance += distance_between(c1, c2)
 
-    distance += distance_between(cities[0], cities[len(cities) -1])
+    distance += distance_between(cities[0], cities[len(cities) - 1])
     return distance
+
 
 def distance_between(city1, city2):
     return math.sqrt(pow(city1[1][0] - city2[1][0], 2) + pow(city1[1][1] - city2[1][1], 2))
+
 
 def initial_population(cities, quantity):
     population = []
     while len(population) < quantity:
         solution = cities.copy()
         random.shuffle(solution)
-        population.append((solution, total_distance(solution)))
+        population.append([solution, None])
     return population
 
 
