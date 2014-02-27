ceabbe7500fc81be5f2d7f7505fbecfdcc0c3729
Alexandre Perez <alexandre.perez92@gmail.com>
2014-02-27 23:19:58 +0100

    Add comments and optimize the distance compute

--- PerezVaucher.py
+++ PerezVaucher.py
@@ -1,3 +1,29 @@
+"""
+    Projet IA
+        “Algorithmes génétiques”
+
+        Alexandre Perez
+        Sébastion Vaucher
+        HE-Arc 2014
+
+    ----------------------------
+
+    Le codage des individus est de type à caractères multiples. L'algorithme commence donc par initialiser la population
+    de base avec comme taille 1000% du nombre de ville de départ. (500 pour 50 villes) Après évaluation de toutes ces
+    solutions, la meilleure est choisie. Puis tant que la meilleure solution n'est pas battue durant 50 fois génération,
+    l'algorithme effectue :
+    1. Sélection :
+        Choisie 30% de la population, les meilleures d'entre eux. Cependant pour éviter de tomber dans un minimum local
+        on ne choisira pas des solutions similaires (dans la distance totale varie de moins de 1 e-6)
+    2. Croisement :
+        Croisement en deux points, tirage de deux point aléatoire et on remplace le chemin entre les deux points de la
+        première solution par ceux de la deuxième et inversement.
+    3. Mutation :
+        Mutation avec la méthode 2opt, ou deux liaisons sont tirées au sort, puis si l'ordre dont sont relier ces 4
+        points (deux côté, donc 4 points) est meilleurs en les inversant alors on mute en les inversant et
+        en inversant le chemin en eux aussi.
+    4. Test si une meilleure solution existe dans la nouvelle population et recommence au point 1.
+"""
 import pygame
 import math
 import random
@@ -17,11 +43,18 @@ city_font = None
 summary_font = None
 
 cities = []
+# Population size percent from the number of cities, here 1000%
 population_size_percent = 1000 / 100
+# Quantity of elite for the selection, here 30% of the population
 elitism_percent = 30 / 100
+# Quantity of solution that we will mutate, here 20% of the population
 mutation_percent = 20 / 100
+# Size of the tournament
 tournament_size = 15
+# Limit for stop generation if no better solution found
 gen_without_better_solution_limit = 50
+# Limit a mutation try before skip it
+limit_mutation_try = 10
 
 
 def cities_from_file(file):
@@ -120,7 +153,7 @@ def ga_solve(file=None, gui=True, maxtime=0):
     gen_without_better_solution = 0
     fittest = None
 
-    while (maxtime == 0 and gen_without_better_solution < gen_without_better_solution_limit)or time.time() - t1 <= maxtime:
+    while (maxtime == 0 and gen_without_better_solution < gen_without_better_solution_limit) or time.time() - t1 <= maxtime:
         if screen is not None:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -156,40 +189,59 @@ def ga_solve(file=None, gui=True, maxtime=0):
 
 
 def evaluate(population):
+    """
+    Evaluate all solutions of the population.
+    """
     for solution in population:
         solution[1] = total_distance(solution[0])
 
 
 def selection(population):
-    elite_quantity = int(len(cities) * elitism_percent) # int(len(population) * elitism_percent)
+    """
+    Call the selection method
+    """
+    elite_quantity = int(len(cities) * elitism_percent)
     #return selection_elites(population, elite_quantity)
     #return selection_tournament(population, elite_quantity)
     return selection_SENGOKU(population, elite_quantity)
 
 
 def crossover(subpopulation, quantity):
+    """
+    Call the crossover method
+    """
     return crossover_two_points(subpopulation, quantity)
 
 
 def mutate(solution):
+    """
+    Call the mutation method
+    """
     #mutate_swap(solution)
     mutate_2opt(solution)
     #mutate_reverse(solution)
 
 
 def selection_elites(population, elite_quantity):
-    # Sort for selection
+    """
+    Return the best N solution from population. Where N = elite_quantity
+    """
     return [population[i] for i in range(0, elite_quantity)]
 
 
 def selection_tournament(population, elite_quantity):
+    """
+    Return N solution from population by apply a tournament selection. Where N = elite_quantity
+    We take X(tournament_size) solution from the population, then we save the best solution, and delete it from the population.
+    We return all solution saved.
+    """
     winners = []
     for i in range(0, elite_quantity):
         competitors = []
         for j in range(0, tournament_size):
-            competitor = population[random.randint(0, len(population)-1)]
+            competitor = population[random.randint(0, len(population) - 1)]
             while competitor in competitors:
-                competitor = population[random.randint(0, len(population)-1)]
+                competitor = population[random.randint(0, len(population) - 1)]
             competitors.append(competitor)
         competitors.sort(key=lambda s: s[1])
         winners.append(competitors[0])
@@ -198,6 +250,11 @@ def selection_tournament(population, elite_quantity):
 
 
 def selection_SENGOKU(population, elite_quantity):
+    """
+    Return N solution from population by apply a natural selection. Where N = elite_quantity
+    We want the best. For keep the diversity of the population, we remove all similar solutions.
+    By removing the similar solutions, we won't go to a local minima.
+    """
     i = 1
     while i < len(population):
         if is_solutions_similar(population[i - 1], population[i]):
@@ -210,6 +267,10 @@ def selection_SENGOKU(population, elite_quantity):
 
 
 def crossover_two_points(subpopulation, quantity):
+    """
+    Apply the two points crossover function on the subpopulation.
+    Return N new solution. Where N = quantity
+    """
     crossed = []
     while len(crossed) < quantity:
         s1 = subpopulation[random.randint(0, len(subpopulation) - 1)]
@@ -235,6 +296,10 @@ def crossover_two_points(subpopulation, quantity):
 
 
 def cross_two_solutions(solution1, solution2, p1, p2):
+    """
+    Apply a crossover with the two solutions passed in parameter.
+    Return two new solutions from the parent (One if the two are the same)
+    """
     middle_cities1 = solution1[p1:p2]
     middle_cities2 = solution2[p1:p2]
 
@@ -254,6 +319,10 @@ def cross_two_solutions(solution1, solution2, p1, p2):
 
 
 def pack_cities(solution, middle_cities, p1, p2):
+    """
+    Pack all the cities to a to the right of the p2 point.
+    Then all points between p1 and p2 are None.
+    """
     for i in range(0, len(solution)):
         if solution[i] in middle_cities:
             solution[i] = None
@@ -275,6 +344,9 @@ def pack_cities(solution, middle_cities, p1, p2):
 
 
 def mutate_swap(solution):
+    """
+    Just swap two randoms points of the solution
+    """
     p1 = random.randint(0, len(solution) - 1)
     p2 = p1
     while p1 == p2:
@@ -285,25 +357,36 @@ def mutate_swap(solution):
 
 
 def mutate_2opt(solution):
+    """
+    Take two random edge, and check if we reverse the path between points of the edges improve out solution.
+    If it's improved, then do the mutation
+    """
     improved = False
-    while not improved:
+    mutation_try = 0
+    while not improved and mutation_try < limit_mutation_try:
         p1 = random.randint(0, len(solution) - 1)
         p2 = p1 + 1 if p1 + 1 < len(solution) else 0
         p3, p4 = p1, p2
         while abs(p1 - p3) < 2 or abs(p2 - p4) < 2:
             p3 = random.randint(0, len(solution) - 1)
             p4 = p3 + 1 if p3 + 1 < len(solution) else 0
-        if distance_between(solution[p1], solution[p2]) + distance_between(solution[p3], solution[p4]) > distance_between(solution[p1], solution[p3]) + distance_between(solution[p2], solution[p4]):
+        if distance_between(solution[p1], solution[p2]) + distance_between(solution[p3],solution[p4]) > \
+                        distance_between(solution[p1], solution[p3]) + distance_between(solution[p2], solution[p4]):
             solution[p2], solution[p3] = solution[p3], solution[p2]
             reverse(solution, p2, p3)
             improved = True
-        elif distance_between(solution[p1], solution[p2]) + distance_between(solution[p3], solution[p4]) > distance_between(solution[p1], solution[p4]) + distance_between(solution[p3], solution[p2]):
+        elif distance_between(solution[p1], solution[p2]) + distance_between(solution[p3], solution[p4]) > \
+                        distance_between(solution[p1], solution[p4]) + distance_between(solution[p3], solution[p2]):
             solution[p2], solution[p4] = solution[p4], solution[p2]
             reverse(solution, p2, p4)
             improved = True
+        mutation_try += 1
 
 
 def mutate_reverse(solution):
+    """
+    Mutate just by reverse a path between two random points
+    """
     p1 = random.randint(0, len(solution) - 1)
     p2 = p1
     while p1 == p2:
@@ -312,6 +395,9 @@ def mutate_reverse(solution):
 
 
 def reverse(solution, p1, p2):
+    """
+    Reverse the path between the two points in the given solution
+    """
     trip = solution[p1:p2] if p1 < p2 else solution[p1:] + solution[:p2]
     while p1 != p2:
         solution[p1] = trip.pop()
@@ -319,10 +405,16 @@ def reverse(solution, p1, p2):
 
 
 def is_solutions_similar(solution1, solution2):
+    """
+    Compare if the two solutions are similar
+    """
     return abs(solution1[1] - solution2[1]) < 1e-6
 
 
 def is_solutions_equal(solution1, solution2):
+    """
+    Compare if the two solutions are equal
+    """
     for i in range(0, len(solution1)):
         if solution1[0][i][0] != solution2[0][i][0]:
             return False
@@ -330,6 +422,9 @@ def is_solutions_equal(solution1, solution2):
 
 
 def total_distance(solution):
+    """
+    Return total distance between all cities, with the distance front the last and the first
+    """
     distance = 0
     c2 = None
     for c in solution:
@@ -343,14 +438,25 @@ def total_distance(solution):
 
 
 def distance_between(city1, city2):
-    return math.sqrt(pow(city1[1][0] - city2[1][0], 2) + pow(city1[1][1] - city2[1][1], 2))
+    """
+    Return the distance between two cities
+    """
+    return math.sqrt((city1[1][0] - city2[1][0]) * (city1[1][0] - city2[1][0]) +
+                     (city1[1][1] - city2[1][1]) * (city1[1][1] - city2[1][1]))
 
 
 def is_solution_in_population(solution, population):
+    """
+    Return true if the solution is in the population
+    """
     return any(abs(sol[1] - solution[1]) < 1e-9 for sol in population)
 
 
 def initial_population(cities, quantity):
+    """
+    Create the initial population from the cities passed in parameter.
+    Each solution is a shuffle of the cities set
+    """
     population = []
     while len(population) < quantity:
         solution = cities.copy()
